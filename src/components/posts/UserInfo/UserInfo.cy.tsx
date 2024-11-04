d5363f0bf84db56e1b7cd74684770955310d3b6c
Johany Quintero <johandquintero@utp.edu.co>
2024-11-04 00:54:28 -0500

    add ci

--- src/components/posts/UserInfo/UserInfo.cy.tsx
+++ src/components/posts/UserInfo/UserInfo.cy.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
 import UserInfo from './UserInfo'
 
 describe('<UserInfo />', () => {
   it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
     cy.mount(<UserInfo />)
   })
 })
