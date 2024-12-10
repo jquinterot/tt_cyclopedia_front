8f8ab79015edd8c95fba7a30d83fa1bd2a830fe5
Johany Quintero <johandquintero@utp.edu.co>
2024-12-10 15:36:38 -0500

    fix import

--- src/components/shared/NavBar/NavBar.test.tsx
+++ src/components/shared/NavBar/NavBar.test.tsx
@@ -1,4 +1,3 @@
-
 import NavBar from './NavBar';
 import { describe, test, expect } from "vitest";
 import {render, screen} from "@testing-library/react";
--- src/components/shared/SideBar/SideBar.test.tsx
+++ src/components/shared/SideBar/SideBar.test.tsx
@@ -1,3 +1,5 @@
+import { describe, test, expect } from "vitest";
+
 describe("NavBar", () => {
     test("should render NavBar", () => {
         expect(1 + 1).toBe(2);
