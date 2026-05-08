e8f80432090ba87a84c6e53ea44736dd0fa3e553
Johany Quintero <johandquintero@utp.edu.co>
2026-05-08 10:44:16 -0500

    fix: update Cypress test for PostDetails skeleton loading state

--- src/pages/PostPage/components/PostDetailsSection/PostDetailsSection.cy.tsx
+++ src/pages/PostPage/components/PostDetailsSection/PostDetailsSection.cy.tsx
@@ -3,14 +3,8 @@ import "../../../../../cypress/support/component";
 import PostDetails from "./PostDetailsSection";
 
 describe("<PostDetails />", () => {
-  it("renders container", () => {
+  it("renders skeleton loading state", () => {
     cy.mount(<PostDetails />);
-    cy.get(".flex.justify-center.items-center").should("exist");
-  });
-
-  it("renders loading spinner", () => {
-    cy.mount(<PostDetails />);
-    cy.get(".animate-spin").should("exist");
-    cy.get('[data-testid="loading-spinner-icon"]').should("exist");
+    cy.get('[data-testid="skeleton-post-detail"]').should("exist");
   });
 });
